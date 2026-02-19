709b497e49f90e5feeb457ac31147c11031b846c
Saudarsh Pal <saudarsh29@gmail.com>
2026-02-19 11:05:09 +0530

    [ADD] endpoint for course stats

--- routes/courseRoutes.ts
+++ routes/courseRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createCourse, deleteCourse, getAllCourse, getCourseLessons, updateCourse } from '../controllers/courseController'
+import { courseStats, createCourse, deleteCourse, getAllCourse, getCourseLessons, updateCourse } from '../controllers/courseController'
 import { authMiddleware } from '../middlewares/authMiddleware'
 
 const router = express.Router()
@@ -11,6 +11,7 @@ router.post('/',createCourse)  // for instructor to create new couse
 router.patch('/:courseId',updateCourse)  // for instructor to update course
 router.get('/lessons/:courseId',getCourseLessons)  // to view the lessons within a course by user
 router.delete('/:courseId',deleteCourse)    // for instructor to delete a  course
+router.get('/stats/:courseId',courseStats)  // for instructor to get the course Stats
 
 
 export default router
